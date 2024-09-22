bd17f7b2e1c78145b9863f4a8f882a4422e97d05
shubhamxcode <shubh.varshneycode@gmail.com>
2024-09-22 13:33:38 +0530

    Type inference

--- 01intro.js
+++ 01intro.js
@@ -1,3 +1,37 @@
 "use strict";
-let x = 23;
-console.log(x);
+// let x:number=23
+// console.log(x)
+Object.defineProperty(exports, "__esModule", { value: true });
+const promises_1 = require("timers/promises");
+// function greetuser(firstname:string, lastname:string,age:number){
+//     console.log(`hello ${lastname}`)
+// }
+// greetuser("shubham","varshney",23)
+// function sum(num1:number,num2:number):number{
+//     return num1+num2
+// }
+// console.log(sum(12,23))
+// function users(age:number):boolean{
+//     if (age>18) {
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// console.log(users(13))
+// let user1:number = 12
+// function user():boolean{
+//     if (user1>18) {
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+// console.log(user())
+function run(fn) {
+    (0, promises_1.setTimeout)(fn, 2000);
+}
+run(function () {
+    console.log("wdwed");
+});
--- 01intro.ts
+++ 01intro.ts
@@ -1,2 +1,49 @@
-let x:number=23
-console.log(x)
+// let x:number=23
+// console.log(x)
+
+import { setTimeout } from "timers/promises"
+
+
+// function greetuser(firstname:string, lastname:string,age:number){
+//     console.log(`hello ${lastname}`)
+// }
+
+// greetuser("shubham","varshney",23)
+
+// function sum(num1:number,num2:number):number{
+//     return num1+num2
+// }
+
+// console.log(sum(12,23))
+
+
+
+// function users(age:number):boolean{
+//     if (age>18) {
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(users(13))
+
+// let user1:number = 12
+
+// function user():boolean{
+//     if (user1>18) {
+//         return true
+//     }else{
+//         return false
+//     }
+// }
+
+// console.log(user())
+
+// function shubham(fn: () => void){
+//     setTimeout(fn, 1000);
+// }
+// shubham(() => {
+//     console.log("shubhadqwd");
+// });
